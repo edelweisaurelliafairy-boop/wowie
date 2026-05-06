029e8d558fb8561464d69dfdeed1287aa3b94efc
edelweisaurelliafairy-boop <edelweis.aurellia.fairy@sekolahbim.sch.id>
2026-05-06 11:18:25 +0700

    Add files via upload

---
+++ pjs3.js
@@ -0,0 +1,9 @@
+function beli(nama) {
+    alert("Kamu membeli: " + nama);
+}
+
+function daftar() {
+    let nama = document.getElementById("nama").value;
+    alert("Pendaftaran berhasil: " + nama);
+    return false;
+}
